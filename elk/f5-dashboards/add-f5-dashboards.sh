7ec9bdf43b5414b4028ce08b17edaebd538eb1bd
Josh Chambers <josh@sparta.isselab.f5.com>
2021-03-12 11:34:25 -0800

    add healthcheck, dashboard script

---
+++ elk/f5-dashboards/add-f5-dashboards.sh
@@ -0,0 +1,13 @@
+#!/bin/ash
+
+jq -s . overview-dashboard.ndjson | jq '{"objects": . }' | \
+curl -k --location --request POST "$KIBANA_URL/api/kibana/dashboards/import" \
+    --header 'kbn-xsrf: true' \
+    --header 'Content-Type: text/plain' -d @- \
+    | jq
+
+jq -s . false-positives-dashboards.ndjson | jq '{"objects": . }' | \
+curl -k --location --request POST "$KIBANA_URL/api/kibana/dashboards/import" \
+    --header 'kbn-xsrf: true' \
+    --header 'Content-Type: text/plain' -d @- \
+    | jq
